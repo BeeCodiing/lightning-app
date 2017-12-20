51e1c2375a9a9a2db2c64687a80ce58a66093317
BeeCoding <rafael@tunning.com.br>
2017-12-20 18:50:06 -0200

    Add comment clarifying what the regex does.

--- packages/lightning-core/helpers/paymentRequest.js
+++ packages/lightning-core/helpers/paymentRequest.js
@@ -4,6 +4,7 @@ export const decoratePaymentRequest = (pr) => {
   return prefix + pr
 }
 
+// Payment requests should start with either lightning: or lightning://
 export const sanitizePaymentRequest = (pr) => {
   return pr.replace(/lightning:(\/\/)?/g, '')
 }
